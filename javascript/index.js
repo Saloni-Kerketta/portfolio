87f580b49a9fde02c7c70919eb70e086350123bf
Saloni Kerketta <salonikerketta48@gmail.com>
2025-06-11 11:17:32 +0530

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,69 @@
+// alert("me");
+// console.log("hello saloni here");
+// console.warn("this is warning");
+// console.error("this is error");
+// var marks = { ravi: 56, saloni: 99, mukesh: 78 };
+// console.log(marks);
+// var arr = [89, 45, 87, "hello"];
+// function avg(a, b) {
+//     return (a + b) / 2;
+// }
+// c1 = avg(2, 5);
+// c2 = avg(8, 9);
+// console.log(c1, c2);
+// var arr = [23, 54, 34, 87];
+// arr.forEach(function (element) { console.log(element); })
+// let str = "saloni is a good girl";
+// let d = str.replace("saloni", "supriya");
+// console.log(str.slice(0, 8));
+// console.log(d, str);
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getFullYear());
+// let elem = document.getElementById('click');
+// console.log(elem.innerHTML);
+// let ele=document.getElementsByClassName("firstSection");
+// console.log(ele.innerHTML);
+// sel=document.querySelector('.firstSection');
+// console.log(sel);
+// sel=document.querySelectorAll('.container');
+// console.log(sel);
+// function clicked(){
+//     console.log('this button was clicked');
+// }
+// window.onload=function(){
+//     console.log('the document is loaded');
+// }
+// right.addEventListener('click',function(){
+//     console.log('clicked on');
+// })
+// right.addEventListener('mouseover',function(){
+//     console.log('mouse over container');
+// })
+
+// function sum(a,b){
+//     return a+b;
+// }
+// sum=(a,b)=>{
+//     return a+b;
+// }
+
+// logkaro=() =>{
+//     console.log('this is log');
+// }
+// setTimeout(logkaro,20);
+
+//  logkaro1=()=>{
+//     document.querySelectorAll('.purple')[0].innerHTML="<b>set interval fired</b>"
+//     console.log("I your log")
+// }
+//  clr=setTimeout(logkaro1,1000);
+
+// localStorage.setItem('name','Saloni');
+// localStorage
+// localStorage.getItem('name')
+
+// obj={name:"harry",length:1}
+// jso=JSON.stringify(obj);
+// console.log(typeof jso)
+// console.log(jso);
